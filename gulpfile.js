0f7e2b6283f4f4aae351468b5109c549930887bf
tezoatlipoca <tezoatlipoca@gmail.com>
2024-05-12 12:55:42 -0400

    Dependency futzing; preparing for submissin to moz dev portal.

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,18 @@
 var gulp = require('gulp');
 
-gulp.task('copy', function() {
+gulp.task('copy-easymde', function() {
     return gulp.src('node_modules/easymde/dist/*')
         .pipe(gulp.dest('lib/easymde/'));
-    
 });
 
-gulp.task('copy', function(){
+gulp.task('copy-showdown', function(){
     return gulp.src('node_modules/showdown/dist/*')
         .pipe(gulp.dest('lib/showdown/'));
-})
+});
+
+gulp.task('watch', function() {
+    gulp.watch('node_modules/easymde/dist/*', gulp.series('copy-easymde'));
+    gulp.watch('node_modules/showdown/dist/*', gulp.series('copy-showdown'));
+});
+
+gulp.task('default', gulp.series('copy-easymde', 'copy-showdown', 'watch'));
